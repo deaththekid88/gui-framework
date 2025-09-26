24c0b2fdd0e6508c98cad7cc5e78a551da563266
deaththekid <deaththekido8888@gmail.com>
2025-09-26 16:22:52 +0900

    EGEngine 클래스 추가 및 초기 설정 구현

---
+++ src/Core/EGEngine.cs
@@ -0,0 +1,65 @@
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using CoreFramework.Platform;
+using CoreFramework.Backend;
+using GuiFramework.Backend;
+using GuiFramework.Event;
+
+namespace GuiFramework.Core;
+
+public class EGEngine : EZEngine
+{
+    private static EGEngine? _instance;
+    private EZBackendGL? _backend;
+
+    public static EGEngine Instance => _instance ??= new EGEngine();
+
+    public EGEngine()
+    {
+        var nativeSettings = new NativeWindowSettings()
+        {
+            Size = new OpenTK.Mathematics.Vector2i(800, 600),
+            Title = "EZEngine OpenGL Backend",
+            Flags = ContextFlags.ForwardCompatible,
+            TransparentFramebuffer = true
+        };
+        _backend = new EZBackendGL(nativeSettings,
+            new Updater(Update), new Renderer(Render), new UIRenderer(UIRender), new ResizedWindow(ResizedWindow));
+
+    }
+
+    public override void Initialize()
+    {
+
+    }
+
+    protected override void Update(float deltaTime)
+    {
+        base.Update(deltaTime);
+    }
+
+    protected override void Render(float alpha)
+    {
+        base.Render(alpha);
+    }
+
+    protected override void UIRender(float alpha)
+    {
+        base.UIRender(alpha);
+    }
+
+    protected override void OnRun()
+    {
+        base.OnRun();
+        //_backend.Run();
+    }
+    
+    private void ResizedWindow(float width, float height)
+    {
+        EventBus.Publish(new ResizedWindowEvent
+        {
+            width = width,
+            height = height
+        });
+    }
+}
