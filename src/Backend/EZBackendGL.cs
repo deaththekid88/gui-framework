3f47467420267fcbaff5afa38d5e5376cb90bc26
deaththekid <deaththekido8888@gmail.com>
2025-09-26 16:22:57 +0900

    EZBackendGL 클래스 추가 및 기본 메서드 구현

---
+++ src/Backend/EZBackendGL.cs
@@ -0,0 +1,58 @@
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Graphics.OpenGL4;
+using CoreFramework.Backend;
+
+namespace GuiFramework.Backend;
+
+public class EZBackendGL : EZBackend
+{
+    public EZBackendGL(NativeWindowSettings nativeWindowSettings, Updater updater, Renderer renderer, UIRenderer uiRenderer, ResizedWindow resizedWindow)
+        : base(nativeWindowSettings, updater, renderer, uiRenderer, resizedWindow)
+    {
+        
+    }
+
+    protected override void OnLoad()
+    {
+        base.OnLoad();
+
+        GL.ClearColor(0f, 0f, 0f, 0f);
+        GL.Enable(EnableCap.Blend);
+        GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
+    }
+
+    protected override void OnUnload()
+    {
+        base.OnUnload();
+        Dispose();
+    }
+
+    protected override void OnResize(ResizeEventArgs e)
+    {
+        base.OnResize(e);
+        GL.Viewport(0, 0, e.Width, e.Height);
+        _resizedWindow(e.Width, e.Height);
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs args)
+    {
+        base.OnRenderFrame(args);
+
+        BeginFrame();
+        // ... Render 모듈 실행 or EventBus.Publish(RenderEvent)
+        EndFrame();
+
+        Context.SwapBuffers(); // ✅ 보통 여기서 호출
+    }
+
+    protected override void BeginFrame()
+    {
+        GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+    }
+    
+    protected override void EndFrame()
+    {
+        
+    }
+}
